abbdb5027381b5d6cf06da6550be58bc9a919186
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:41:29 +0100

    Cleanup tests

--- tests/test_sale_shipment_comment.py
+++ tests/test_sale_shipment_comment.py
@@ -7,30 +7,20 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class SaleShipmentCommentTestCase(unittest.TestCase):
-    '''
-    Test Sale Shipment Comment module.
-    '''
+    'Test Sale Shipment Comment module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_shipment_comment')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_shipment_comment')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
