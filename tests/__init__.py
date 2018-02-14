e16c1a034ca0ed0a149d3d0b3827a81e3f978e66
C?dric Krier <ced@b2ck.com>
2018-02-14 17:09:55 +0100

    Update to 4.7

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,9 @@
 # This file is part of the sale_shipment_comment module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_sale_shipment_comment import suite
+try:
+    from trytond.modules.sale_shipment_comment.tests.test_sale_shipment_comment import suite
+except ImportError:
+    from .test_sale_shipment_comment import suite
 
 __all__ = ['suite']
