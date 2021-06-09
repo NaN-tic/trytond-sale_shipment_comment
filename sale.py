9a6a9e60c1836c287d15f8ac4179a6ef1c669252
Raimon Esteve <raimonesteve@gmail.com>
2021-06-09 10:10:01 +0200

    Add readonly state
    #044973

--- sale.py
+++ sale.py
@@ -3,13 +3,18 @@
 # copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 
 __all__ = ['Sale']
 
 
 class Sale(metaclass=PoolMeta):
     __name__ = 'sale.sale'
-    shipment_comment = fields.Text('Shipment Comment')
+    shipment_comment = fields.Text('Shipment Comment',
+        states={
+            'readonly': Eval('state') != 'draft',
+            },
+        depends=['state'])
 
     def _get_shipment_sale(self, Shipment, key):
         key += (('comment', self.shipment_comment),)
