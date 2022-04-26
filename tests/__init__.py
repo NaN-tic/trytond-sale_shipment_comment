107355767ceab3c0d4f5759782122ca060732e38
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:08:52 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part of the sale_shipment_comment module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.sale_shipment_comment.tests.test_sale_shipment_comment import suite
-except ImportError:
-    from .test_sale_shipment_comment import suite
-
-__all__ = ['suite']
